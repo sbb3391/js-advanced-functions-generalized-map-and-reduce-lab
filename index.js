a5af80abd3173361976be66f20c934b46cd71a8b
sbb3391 <sbb33391@gmail.com>
2021-06-11 21:33:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your functions here
+
+function map(items, callback) {
+  let newArr = []
+  for (const item of items) {
+    newArr.push(callback(item))
+  }
+  return newArr
+}
+
+function reduce(items, callback, startingValue = 0) {
+  let total;
+  
+  if (total) {
+    total = startingValue;
+    for (const item of items) {
+      total = callback(item, total)
+    }  
+
+    return total
+  } else {
+    total = items[0]
+
+    for (const item of items) {
+      total = callback(item, total)
+    }
+
+    return total;
+  }
+
+}
